a76c0d762a7ea37837336e979999de3602cb5397
liruowei0919 <43594563+liruowei0919@users.noreply.github.com>
2022-08-16 17:48:06 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+
+__author__ = 'Oliver Banse'
+
+
+# IDEA
+# Import and Initialization
+import pygame
+
+from classes.Screen import Screen
+from classes.Game import Game
+
+pygame.init()
+
+# Display configuration
+screen = Screen('Hit the Mole!', 800, 600)
+# Entities
+game = Game(screen)
+# Run Game
+game.start()
+
+x = np.
+
+#if __name__ == '__main__':
+#    game.start()
+
+print("x")
